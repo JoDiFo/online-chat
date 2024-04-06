1828f2e8949b0712c174ccf11e95278776b8cb8a
alexfokin <sasafokin2@gmail.com>
2024-04-06 14:42:22 +0300

    feat: insert layout into App and applied styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,229 @@
-import { useState } from "react";
+import "./styles/index.css";
 
 function App() {
-  const [value, setValue] = useState(0);
-
   return (
-    <div>
-      <h1>{value}</h1>
-      <button onClick={() => setValue((prev) => prev + 1)}>Click</button>
+    <div className="content">
+      {/* <!-- SIDEBAR --> */}
+      <div className="sidebar">
+        <button className="burger-menu"></button>
+      </div>
+
+      {/* <!-- CHAT LIST --> */}
+      <div className="chat-list">
+        <div className="search-bar">
+          <button className="search-icon"></button>
+        </div>
+
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <div className="chat-item">
+          <div className="user-avatar"></div>
+          <div className="username">username</div>
+        </div>
+        <button className="add-chat"></button>
+      </div>
+
+      {/* <!-- CHAT ZONE --> */}
+      <div className="chat-zone">
+        <div className="chat-head">
+          <div className="username">username</div>
+          <button className="search-icon"></button>
+        </div>
+
+        <div className="chat-messages">
+          <div className="message received">
+            <p className="message-text">Lorem.</p>
+          </div>
+          <div className="message received">
+            <p className="message-text">Lorem, ipsum dolor.</p>
+          </div>
+          <div className="message sent">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+              fugit.
+            </p>
+          </div>
+          <div className="message received">
+            <p className="message-text">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Cupiditate facilis, ab, eos molestiae repellendus natus rem aut
+              illum officia hic saepe recusandae eaque, provident vel. Libero
+              commodi aliquid hic praesentium!
+            </p>
+          </div>
+          <div className="message received">
+            <p className="message-text">Lorem.</p>
+          </div>
+          <div className="message received">
+            <p className="message-text">Lorem, ipsum dolor.</p>
+          </div>
+          <div className="message sent">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+              fugit.
+            </p>
+          </div>
+          <div className="message received">
+            <p className="message-text">Lorem.</p>
+          </div>
+          <div className="message sent">
+            <p className="message-text">Lorem, ipsum dolor.</p>
+          </div>
+          <div className="message received">
+            <p className="message-text">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Cupiditate facilis, ab, eos molestiae repellendus natus rem aut
+              illum officia hic saepe recusandae eaque, provident vel. Libero
+              commodi aliquid hic praesentium!
+            </p>
+          </div>
+          <div className="message sent">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+              fugit.
+            </p>
+          </div>
+          <div className="message sent">
+            <p className="message-text">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Cupiditate facilis, ab, eos molestiae repellendus natus rem aut
+              illum officia hic saepe recusandae eaque, provident vel. Libero
+              commodi aliquid hic praesentium!
+            </p>
+          </div>
+          <div className="message sent">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+              fugit.
+            </p>
+          </div>
+          <div className="message sent">
+            <p className="message-text">Lorem.</p>
+          </div>
+          <div className="message sent">
+            <p className="message-text">Lorem, ipsum dolor.</p>
+          </div>
+          <div className="message received">
+            <p className="message-text">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Cupiditate facilis, ab, eos molestiae repellendus natus rem aut
+              illum officia hic saepe recusandae eaque, provident vel. Libero
+              commodi aliquid hic praesentium!
+            </p>
+          </div>
+          <div className="message sent">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+              fugit.
+            </p>
+          </div>
+          <div className="message sent">
+            <p className="message-text">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Cupiditate facilis, ab, eos molestiae repellendus natus rem aut
+              illum officia hic saepe recusandae eaque, provident vel. Libero
+              commodi aliquid hic praesentium!
+            </p>
+          </div>
+        </div>
+
+        <div className="composing-area">
+          <button className="clip-icon"></button>
+          <input
+            className="message-bar"
+            type="text"
+            placeholder="Write a message..."
+          />
+          <button className="emoji-button"></button>
+          <button className="send-button"></button>
+        </div>
+      </div>
     </div>
   );
 }
