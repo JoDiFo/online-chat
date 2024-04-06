e4450b07b3b4cb3b59ea3610c85a656089cc9ec9
alexfokin <sasafokin2@gmail.com>
2024-04-06 14:34:51 +0300

    feat: configure eslinter and fix errors

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react", "prettier"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "prettier/prettier": ["error"],
+    "react/react-in-jsx-scope": "off",
+  },
+  ignorePatterns: ["eslint.cjs"],
+};
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
-    <div>App</div>
-  )
+    <div>
+      <h1>{value}</h1>
+      <button onClick={() => setValue((prev) => prev + 1)}>Click</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
