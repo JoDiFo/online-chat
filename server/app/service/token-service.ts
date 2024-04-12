3debd5b652599d19e22521a9aa997d70d45411c5
alexfokin <sasafokin2@gmail.com>
2024-04-12 18:38:13 +0300

    feat: add linter to server

--- server/app/service/token-service.ts
+++ server/app/service/token-service.ts
@@ -3,10 +3,10 @@ import "dotenv/config";
 import tokenModel from "../models/token-model";
 
 class TokenService {
-  generateTokens(payload: any) {
+  generateTokens(payload: object) {
     const accessSecret = process.env.JWT_ACCESS_SECRET;
     if (!accessSecret) {
-      throw new Error(`access secret is not defined`);
+      throw new Error("access secret is not defined");
     }
     const accessToken = jwt.sign(payload, accessSecret, {
       expiresIn: "15m",
@@ -14,7 +14,7 @@ class TokenService {
 
     const refreshSecret = process.env.JWT_REFRESH_SECRET;
     if (!refreshSecret) {
-      throw new Error(`refresh secret is not defined`);
+      throw new Error("refresh secret is not defined");
     }
     const refreshToken = jwt.sign(payload, refreshSecret, {
       expiresIn: "15d",
