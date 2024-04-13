5fd6fd87b36f968f0671f7b685e9d539a3733c04
alexfokin <sasafokin2@gmail.com>
2024-04-13 18:22:16 +0300

    feat: add checkAuthorization

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,8 +3,18 @@ import { ChatList } from "@/widgets/ChatList";
 import { ChatArea } from "@/widgets/ChatArea";
 
 import "./styles/index.scss";
+import { useContext, useEffect } from "react";
+import { Context } from "..";
 
 function App() {
+  const { store } = useContext(Context);
+
+  useEffect(() => {
+    if (localStorage.getItem("refreshToken")) {
+      store.checkAuth();
+    }
+  });
+
   return (
     <div className="content">
       <Sidebar />
--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -1,9 +1,13 @@
 import { IUser } from "@/models/User";
-import AuthService from "../Services/AuthService";
+import AuthService from "@/app/Services/AuthService";
+import axios from "axios";
+import { API_URL } from "@/app/http";
+import { IAuthResponse } from "@/models/response/AuthResponse";
 
 export default class Store {
   user = {} as IUser;
   isAuth = false;
+  isLoading = false;
 
   setAuth(value: boolean) {
     this.isAuth = value;
@@ -11,6 +15,10 @@ export default class Store {
 
   setUser(user: IUser) {
     this.user = user;
+  }
+
+  setLoading(value: boolean) {
+    this.isLoading = value;
   }
 
   async login(email: string, password: string) {
@@ -45,6 +53,23 @@ export default class Store {
       this.setUser({} as IUser);
     } catch (error: any) {
       console.log(error.response?.data?.message);
+    }
+  }
+
+  async checkAuth() {
+    this.setLoading(true);
+    try {
+      const response = await axios.get<IAuthResponse>(`${API_URL}/refresh`, {
+        withCredentials: true,
+      });
+      console.log(response);
+      localStorage.setItem("token", response.data.accessToken);
+      this.setAuth(true);
+      this.setUser(response.data.user);
+    } catch (error: any) {
+      console.log(error.response?.data?.message);
+    } finally {
+      this.setLoading(false);
     }
   }
 }
