c1542b8f81e3e441a16a383e4307ace08db51d80
alexfokin <sasafokin2@gmail.com>
2024-04-13 18:30:34 +0300

    feat: add request interceptor

--- client/src/app/http/index.ts
+++ client/src/app/http/index.ts
@@ -1,3 +1,4 @@
+import { IAuthResponse } from "@/models/response/AuthResponse";
 import axios from "axios";
 
 export const API_URL = "http://localhost:5000/api";
@@ -11,5 +12,31 @@ $api.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
   return config;
 });
+
+$api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response.status === 401 &&
+      error.config &&
+      originalRequest._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      try {
+        const response = await axios.get<IAuthResponse>(`${API_URL}/refresh`, {
+          withCredentials: true,
+        });
+        localStorage.setItem("token", response.data.accessToken);
+        return $api.request(originalRequest);
+      } catch (error) {
+        console.log("not authorized");
+      }
+    }
+    throw error;
+  },
+);
 
 export default $api;
