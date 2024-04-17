9c19af6f4013bfd4b528556637bf58af11e1c857
alexfokin <sasafokin2@gmail.com>
2024-04-17 12:32:39 +0300

    fix: add username at chat header

--- client/src/widgets/ChatHeader/ui/ChatHeader.tsx
+++ client/src/widgets/ChatHeader/ui/ChatHeader.tsx
@@ -1,9 +1,14 @@
+import { useAppSelector } from "@/app/redux/hooks";
+import { selectUser } from "@/entities/user";
+
 import cls from "./ChatHeader.module.scss";
 
 export const ChatHeader = () => {
+  const username = useAppSelector(selectUser).username;
+
   return (
     <div className={cls.ChatHeader}>
-      <div className={cls.Username}>username</div>
+      <div className={cls.Username}>{username}</div>
       <button className={cls.SearchIcon}></button>
     </div>
   );
